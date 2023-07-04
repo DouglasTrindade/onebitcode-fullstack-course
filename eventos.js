b211b8b8f0b5938b28fbbfe4cc8fa0cdd11bb7bf
Douglas Trindade <douglastrindade22@hotmail.com>
2023-07-04 14:16:28 -0300

    feat: events class

---
+++ eventos.js
@@ -0,0 +1,29 @@
+const register = (ev) => {
+  console.log(ev);
+
+  const sectionElement = ev.currentTarget.parentNode;
+
+  const username = sectionElement.children.username.value;
+  const password = sectionElement.children.password.value;
+  const passwordConfirmation =
+    sectionElement.children.passwordConfirmation.value;
+
+  if (password === passwordConfirmation) {
+    alert(`Usuário ${username} registrado com sucesso!`);
+  } else {
+    alert("As senhas não conferem");
+  }
+};
+
+const button = document.getElementById("register-button");
+
+button.addEventListener("click", register);
+
+const removeListener = () => {
+  button.removeEventListener("click", register);
+  alert("Event Removed");
+};
+
+button.addEventListener("mouseover", (ev) => {
+  console.log(ev.currentTarget);
+});
